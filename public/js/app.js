c7a75d58f37b3c36bdf9d96440636560cc719fe9
greggt94 <gtjunior94@gmail.com>
2017-06-03 11:05:51 -0400

    updated file structure

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
+    // initialize slider for landing page
     $('.slider').slider();
+    // initialize mobile menu
     $('.button-collapse').sideNav({
-        menuWidth: 300,
+        menuWidth: 300, // Menu width
         edge: 'left',
-        draggable: true
+        draggable: true // Makes menu draggable with touchscreen
     });
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 50, // Creates a dropdown of 15 years to control year
-        max: new Date(2017, 5, 10)
+        max: new Date(2017, 5, 10) // Sets max date to today
     });
 
 
