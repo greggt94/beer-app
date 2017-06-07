f9434f76b9e1a1f4108394745d6b69c4db06a97a
greggt94 <gtjunior94@gmail.com>
2017-06-06 20:30:55 -0400

    updated handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,24 +4,28 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  app.get("/login", function(req, res) {
-    res.render("login");
-  });
+    app.get("/login", function(req, res) {
+        res.render("login");
+    });
 
-  app.get("/main", function(req, res) {
-    res.sendFile("beer"));
-  });
+    app.get("/main", function(req, res) {
+        res.render("beer");
+    });
 
-  // // blog route loads blog.html
-  // app.get("/blog", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/blog.html"));
-  // });
-  //
-  // // authors route loads author-manager.html
-  // app.get("/authors", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/author-manager.html"));
-  // });
+    app.get("/users", function(req, res) {
+        res.render("userManager");
+    });
+
+    // // blog route loads blog.html
+    // app.get("/blog", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/blog.html"));
+    // });
+    //
+    // // authors route loads author-manager.html
+    // app.get("/authors", function(req, res) {
+    //   res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+    // });
 
 };
