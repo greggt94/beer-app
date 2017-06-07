34e59499f3d4548e32a436556eb775a68f302d24
greggt94 <gtjunior94@gmail.com>
2017-06-06 20:18:42 -0400

    added handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,12 +6,12 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
+  app.get("/login", function(req, res) {
+    res.render("login");
   });
 
   app.get("/main", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/main.html"));
+    res.sendFile("beer"));
   });
 
   // // blog route loads blog.html
